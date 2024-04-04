0ef559350d55b98b2544ddb7d045795ec7ccae96
Abdulaziz Albahar <abdulaziz@albahar.net>
2024-04-04 18:19:35 -0400

    Initial Commit

---
+++ Clother/ClotherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClotherApp.swift
+//  Clother
+//
+//  Created by Abdulaziz Albahar on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ClotherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
