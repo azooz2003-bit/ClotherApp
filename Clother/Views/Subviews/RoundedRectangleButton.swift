a097e56e1622862cb2c0822c9993eb40bdc646b9
Abdulaziz Albahar <abdulaziz@albahar.net>
2024-04-10 18:51:50 -0400

    changed corner radius and frame dimensions

--- Clother/Views/Subviews/RoundedRectangleButton.swift
+++ Clother/Views/Subviews/RoundedRectangleButton.swift
@@ -17,16 +17,13 @@ struct RoundedRectangleButton: View {
                 Text(buttonText)
                     .fontWeight(.medium)
                     .foregroundColor(Color(red: 0.529, green: 0.553, blue: 0.616, opacity: 0.60))
-                    .padding(.horizontal, 20)
-                    .padding(.vertical, 10)
+                    .frame(width: 150, height: 50)
                     .background(Color(red: 0.957, green: 0.965, blue: 0.988, opacity: 1.0))
-                    .cornerRadius(10)
+                    .cornerRadius(15)
                     .overlay(
                         RoundedRectangle(cornerRadius: 10)
                             .stroke(Color(red: 0.529, green: 0.553, blue: 0.616, opacity: 0.20), lineWidth: 3)
                     )
-                    .shadow(color: Color(red: 0.957, green: 0.965, blue: 0.988, opacity: 0.50), radius: 2
-                            , x: 0, y: 0)
                     .font(.headline)
                     .fontDesign(.monospaced)
             }
@@ -39,4 +36,5 @@ struct RoundedRectangleButton: View {
     RoundedRectangleButton(onPress: {print("Button Pressed")}, buttonText: "Retake")
         .previewLayout(.sizeThatFits)
         .padding()
+        
 }
