c77ac3e72d88e4801573ed1d0d840fdeef3d7938
ronitjain04 <ronit.jain@gmail.com>
2024-04-12 01:56:55 -0400

    possible way to split the button when taking a picture instead of having a whole new view for the upload and take photo buttons

--- Clother/Views/HomeView.swift
+++ Clother/Views/HomeView.swift
@@ -10,15 +10,48 @@ import SwiftUI
 struct HomeView: View {
     // TODO: this is the entry point of your app. Hence, you must do the following in this view:
     /*
-        - Instantiate the app's view models, which will also be shared with all other views.
-        - Build the home view UI, which includes the toggle between the 'Clothes' section and 'Outfits' section.
-        - Observe the UI components implemented under subviews folder. Do not reimplement a subview that has been built, use the subviews given to you as much as possible.
-        - Navigation should also work in this view. This screen can jump to multiple views as shown in Figma. Utilize the view model functions and variables for navigation. Since this is the root view, don't forget to wrap it with a navigation stack.
-        - Your view models should be injected into the navigation and subview hierarchies as an 'environment object'.
+     - Instantiate the app's view models, which will also be shared with all other views.
+     - Build the home view UI, which includes the toggle between the 'Clothes' section and 'Outfits' section.
+     - Observe the UI components implemented under subviews folder. Do not reimplement a subview that has been built, use the subviews given to you as much as possible.
+     - Navigation should also work in this view. This screen can jump to multiple views as shown in Figma. Utilize the view model functions and variables for navigation. Since this is the root view, don't forget to wrap it with a navigation stack.
+     - Your view models should be injected into the navigation and subview hierarchies as an 'environment object'.
      */
-
+    @State var displayCameraOptions: Bool = false
+    
     var body: some View {
-        Text("")
+        HStack {
+            if (displayCameraOptions) {
+                VStack {
+                    Spacer()
+                    RoundedButton(onPress: {
+                        print("Button Pressed")
+                    }, icon: "square.and.arrow.up")
+                    .padding()
+                }
+                .background(Color.white)
+            }
+            VStack {
+                Spacer()
+                RoundedButton(onPress: {
+                    withAnimation(.smooth) {
+                        displayCameraOptions.toggle()
+                    }
+                }, icon: displayCameraOptions ? "x.circle.fill" : "camera")
+                .padding()
+            }
+            .background(Color.white)
+            if (displayCameraOptions) {
+                VStack {
+                    Spacer()
+                    RoundedButton(onPress: {
+                        print("Button Pressed")
+                    }, icon: "camera")
+                    .padding()
+                }
+                .background(Color.white)
+            }
+            
+        }
     }
     
 }
