05fb472c4a9fe87fd4cc9e104bb4d728677b5354
ronitjain04 <ronit.jain@gmail.com>
2024-04-12 07:22:28 -0400

    fixed changes

--- Clother/Views/Subviews/ClosetGrid.swift
+++ Clother/Views/Subviews/ClosetGrid.swift
@@ -15,53 +15,15 @@ struct ClosetGrid<T>: View where T : ClosetItem  {
     let closetItems: [T]
     
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: [.init(.adaptive(minimum: 140, maximum: .infinity), spacing: 0)], spacing: 40) {
-                ForEach(closetItems.indices) { idx in
-                    Button(action: {
-                        onItemPress(closetItems[idx])
-                    }) {
-                        ZStack (alignment: .bottomLeading) {
-                            VStack {
-                                ZStack {
-                                    RoundedRectangle(cornerRadius: 15)
-                                        .foregroundColor(Color(red: 0.96, green: 0.96, blue: 0.99))
-                                    
-                                    if let imageData = closetItems[0].displayImage, let uiImage = UIImage(data: imageData) {
-                                        Image(uiImage: uiImage)
-                                            .resizable()
-                                            .aspectRatio(contentMode: .fit)
-                                            .frame(width: 100, height: 100)
-                                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                                    } else {
-                                        Image(systemName: "photo")
-                                            .resizable()
-                                            .aspectRatio(contentMode: .fit)
-                                            .frame(width: 100, height: 100)
-                                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                                    }
-                                }
-                                .frame(width:150, height:150)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 15)
-                                        .stroke(Color(red: 0.53, green: 0.55, blue: 0.62).opacity(0.25), lineWidth: 6)
-                                )
-                            }
-                            HStack {
-                                Text(closetItems[0].name)
-                                    .bold()
-                                    .fontDesign(.monospaced)
-                                    .foregroundColor(Color(red: 0.53, green: 0.55, blue: 0.62))
-                                    .font(.system(size: 16))
-                            }
-                            .offset(x:12, y:-6)
-                        }
+            ScrollView {
+                LazyVGrid(columns: [.init(.adaptive(minimum: 140, maximum: .infinity), spacing: -10)], spacing: 18) {
+                    ForEach(closetItems.indices) { idx in
+                        ClosetItemView(closetItem: closetItems[idx], onPress: onItemPress)
                     }
                 }
             }
         }
     }
-}
 
 #Preview {
     ClosetGrid<ClothingItem>(onItemPress: {
--- Clother/Views/Subviews/ClosetItemView.swift
+++ Clother/Views/Subviews/ClosetItemView.swift
@@ -15,12 +15,51 @@ struct ClosetItemView<T>: View where T : ClosetItem {
     let onPress: (T) -> Void
     
     var body: some View {
-        Text("")
+        Button(action: {
+            onPress(closetItem)
+        }) {
+            ZStack (alignment: .bottomLeading) {
+                VStack {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 15)
+                            .foregroundColor(Color(red: 0.96, green: 0.96, blue: 0.99))
+                        
+                        if let imageData = closetItem.displayImage, let uiImage = UIImage(data: imageData) {
+                            Image(uiImage: uiImage)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 125, height: 125)
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                        } else {
+                            Image(systemName: "photo")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 125, height: 125)
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                        }
+                    }
+                    .frame(width:175, height:175)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 15)
+                            .stroke(Color(red: 0.53, green: 0.55, blue: 0.62).opacity(0.25), lineWidth: 6)
+                    )
+                }
+                HStack {
+                    Text(closetItem.name)
+                        .bold()
+                        .fontDesign(.monospaced)
+                        .foregroundColor(Color(red: 0.53, green: 0.55, blue: 0.62))
+                        .font(.system(size: 16))
+                }
+                .offset(x:14, y:-8)
+            }
+        }
     }
 }
 
+
 #Preview {
-    ClosetItemView(closetItem: OutfitItem.sample, onPress: {
+    ClosetItemView(closetItem: ClothingItem.sample, onPress: {
         print($0)
     })
 }
