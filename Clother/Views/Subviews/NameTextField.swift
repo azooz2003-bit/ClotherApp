d45c4b4ae0fe835f0941024c71edccbb3b16ea9e
Leo Ouyang <layoouyang@gmail.com>
2024-04-13 20:14:14 -0400

    leo's changes of nametextfield

--- Clother/Views/Subviews/NameTextField.swift
+++ Clother/Views/Subviews/NameTextField.swift
@@ -10,10 +10,20 @@ import SwiftUI
 struct NameTextField: View {
     
     // TODO: this variable is missing something that will allow us to share it's value with the superview.
-    var nameInput: String
+    @State var nameInput: String = ""
     
     var body: some View {
-        Text("")
+        ZStack {
+            RoundedRectangle(cornerRadius: 15)
+                .stroke(Color(red: 0.529, green: 0.553, blue: 0.616, opacity: 0.20), lineWidth:3)
+            
+            TextField("Enter Name...", text: $nameInput)
+                .textContentType(.givenName)
+                .fontDesign(.monospaced)
+                .padding(.leading, 20)
+            
+        }.frame(width: 360, height: 40)
+            .background(Color(red: 0.957, green: 0.965, blue: 0.988, opacity: 1.0))
     }
 }
 
